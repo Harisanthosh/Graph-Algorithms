02a705ae2a60b3e2dafe8728beaaeeb68ddcf0b5
Hari Santhosh <harisanthosh94@gmail.com>
2019-10-09 11:06:15 +0530

    Retrieve nodes from neo4j

--- neo4j_tester.py
+++ neo4j_tester.py
@@ -13,6 +13,13 @@ class HelloWorldExample(object):
             greeting = session.write_transaction(self._create_and_return_greeting, message)
             print(greeting)
 
+    def get_greetings(self):
+        with self._driver.session() as session:
+            gnodes = session.write_transaction(self._get_greetings)
+            for item in gnodes:
+                print(f'{item}')
+            print(gnodes)
+
     @staticmethod
     def _create_and_return_greeting(tx, message):
         result = tx.run("CREATE (a:Greeting) "
@@ -20,6 +27,12 @@ class HelloWorldExample(object):
                         "RETURN a.message + ', from node ' + id(a)", message=message)
         return result.single()[0]
 
+    @staticmethod
+    def _get_greetings(tx):
+        rxx = tx.run("MATCH (n:Greeting)"
+                     "RETURN n LIMIT 25")
+        return rxx
 
 cx = HelloWorldExample("bolt://localhost:7687","neo4j","123graph")
 cx.print_greeting("Champion")
+cx.get_greetings()
