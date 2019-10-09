25147c7fe71cffd4f9be1b7e0a0b7f0af7061024
Hari Santhosh <harisanthosh94@gmail.com>
2019-10-09 18:05:47 +0530

    Cypher query for relations and deleting the node

--- neo4j_tester.py
+++ neo4j_tester.py
@@ -8,9 +8,9 @@ class HelloWorldExample(object):
     def close(self):
         self._driver.close()
 
-    def print_greeting(self, message):
+    def print_greeting(self, message,name,weightclass):
         with self._driver.session() as session:
-            greeting = session.write_transaction(self._create_and_return_greeting, message)
+            greeting = session.write_transaction(self._create_and_return_greeting, message,name,weightclass)
             print(greeting)
 
     def get_greetings(self):
@@ -21,10 +21,12 @@ class HelloWorldExample(object):
             print(gnodes)
 
     @staticmethod
-    def _create_and_return_greeting(tx, message):
+    def _create_and_return_greeting(tx, message,name,weightclass):
         result = tx.run("CREATE (a:Greeting) "
                         "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
+                        "SET a.name = $name "
+                        "SET a.weightclass = $weightclass "
+                        "RETURN a.message + ', from node ' + id(a)", message=message,name=name,weightclass=weightclass)
         return result.single()[0]
 
     @staticmethod
@@ -34,5 +36,21 @@ class HelloWorldExample(object):
         return rxx
 
 cx = HelloWorldExample("bolt://localhost:7687","neo4j","123graph")
-cx.print_greeting("Champion")
+cx.print_greeting("Champion","Stipe Miocic","265lbs")
 cx.get_greetings()
+
+
+
+"""
+For Establishing Relations
+MATCH (n:Greeting {name:"Khabib"}) 
+MATCH (t:Greeting {name:"Kamaru Usman"})
+CREATE (n)-[r:MANAGED_BY {agent:"Ali abdelaziz"}]->(t)
+RETURN n,r,t
+
+
+For Delete
+MATCH (p:Person) where ID(p)=1
+OPTIONAL MATCH (p)-[r]-() //drops p's relations
+DELETE r,p
+"""
