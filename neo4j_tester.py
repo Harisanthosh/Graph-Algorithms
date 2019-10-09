b0a5c5d65015d2446fbb4aa86ce435150a95f5ac
Hari Santhosh <harisanthosh94@gmail.com>
2019-10-09 19:20:47 +0530

    Degree Centrality for nodes

--- neo4j_tester.py
+++ neo4j_tester.py
@@ -13,9 +13,21 @@ class HelloWorldExample(object):
             greeting = session.write_transaction(self._create_and_return_greeting, message,name,weightclass)
             print(greeting)
 
+    def print_manager(self, message,name):
+        with self._driver.session() as session:
+            greeting = session.write_transaction(self._create_manager, message,name)
+            print(greeting)
+
     def get_greetings(self):
         with self._driver.session() as session:
             gnodes = session.write_transaction(self._get_greetings)
+            for item in gnodes:
+                print(f'{item}')
+            print(gnodes)
+
+    def get_degree_centrality(self):
+        with self._driver.session() as session:
+            gnodes = session.write_transaction(self._get_centrality)
             for item in gnodes:
                 print(f'{item}')
             print(gnodes)
@@ -30,14 +42,32 @@ class HelloWorldExample(object):
         return result.single()[0]
 
     @staticmethod
+    def _create_manager(tx, message, name):
+        result = tx.run("CREATE (a:Agent) "
+                        "SET a.message = $message "
+                        "SET a.name = $name "
+                        "RETURN a.message + ', from node ' + id(a)", message=message, name=name)
+        return result.single()[0]
+
+    @staticmethod
     def _get_greetings(tx):
         rxx = tx.run("MATCH (n:Greeting)"
                      "RETURN n LIMIT 25")
         return rxx
 
+    @staticmethod
+    def _get_centrality(tx):
+        rxx = tx.run("CALL algo.degree.stream(null, 'MANAGED_BY', {direction: 'incoming'})"
+                     "YIELD nodeId, score"
+                     "RETURN algo.asNode(nodeId).name AS name, score AS clients"
+                     "ORDER BY clients DESC")
+        return rxx
+
 cx = HelloWorldExample("bolt://localhost:7687","neo4j","123graph")
-cx.print_greeting("Champion","Stipe Miocic","265lbs")
+#cx.print_greeting("Champion","Nate Diaz","165lbs")
+#cx.print_manager("Manager","Audie Attar")
 cx.get_greetings()
+cx.get_degree_centrality()
 
 
 
@@ -48,9 +78,22 @@ MATCH (t:Greeting {name:"Kamaru Usman"})
 CREATE (n)-[r:MANAGED_BY {agent:"Ali abdelaziz"}]->(t)
 RETURN n,r,t
 
+MATCH (n:Greeting {name:"John Jones"}) 
+MATCH (t:Agent {name:"Ali Abdelaziz"})
+CREATE (n)-[r:MANAGED_BY]->(t)
+RETURN n,r,t
 
 For Delete
 MATCH (p:Person) where ID(p)=1
 OPTIONAL MATCH (p)-[r]-() //drops p's relations
 DELETE r,p
+
+MATCH ()-[r:RELEASED]-() 
+DELETE r
+
+Degree Centrality Theorem
+CALL algo.degree.stream("", "MANAGED_BY", {direction: "incoming"})
+YIELD nodeId, score
+RETURN algo.asNode(nodeId).name AS name, score AS clients
+ORDER BY clients DESC
 """
