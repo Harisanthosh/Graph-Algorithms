4a970cb46afc76aa5c4ab211f4e296a2e31f18cf
Hari Santhosh <harisanthosh94@gmail.com>
2019-10-11 15:31:55 +0530

    NeoDash dashboard origin

---
+++ dash_tester.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import requests
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+    'usa-agricultural-exports-2011.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+app.layout = html.Div(children=[
+    html.H1(children='Neo4j & Dash'),
+
+    html.Div(children='''
+        NeoDash: A web application framework for Python customized for Python and Neo4j.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    ),
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df),
+    html.Button('Run Neo4j', id='button_neo'),
+    html.Div(id="load_div", children='loading..')
+
+])
+
+@app.callback(dash.dependencies.Output('load_div','children'),[dash.dependencies.Input('button_neo', 'n_clicks')])
+def update_output(n_clicks):
+    print(f'The button was clicked ${n_clicks} times')
+    resp = requests.get('http://localhost:8000/getrank').json()
+    print(resp)
+    return 'The button was clicked {} times and the response is {}'.format(
+        n_clicks,
+        resp
+    )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
